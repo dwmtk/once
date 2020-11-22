747c02e7ece88d5495002f23633b33023eb670c6
MOTOKI SAKASHITA <yuki.momayu@gmail.com>
2020-11-22 15:20:31 +0900

    微修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use App\Mail\SendRegisterMail;
 use Illuminate\Support\Facades\Auth;
+use Mail;
 
 class RegisterController extends Controller
 {
